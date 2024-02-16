6e70388461b0670c392de3d29b517108e4fe93fa
Mario <maalnu93@hotmail.com>
2024-02-16 13:58:08 +0100

    Empezando Modales

--- build/js/app.js
+++ build/js/app.js
@@ -1,29 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-  iniciarApp();
+document.addEventListener('DOMContentLoaded', function () {
+    iniciarApp();
 });
 
 function iniciarApp() {
-  crearGaleria();
+    crearGaleria();
 
-  let yearSpan = document.getElementById("year");
+    let yearSpan = document.getElementById("year");
 
     let currentYear = new Date().getFullYear();
     yearSpan.textContent = currentYear;
 }
 
 function crearGaleria() {
-  const galeria = document.querySelector('.galeria-imagenes');
+    const galeria = document.querySelector('.galeria-imagenes');
 
     for (let i = 1; i <= 12; i++) {
-      const imagen = document.createElement('picture');
+        const imagen = document.createElement('picture');
         imagen.innerHTML = `
-            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
-            <source srcset="build/img/thumb/${i}.jpg" type="image/jpeg">
-            <img loading="lazy" width="200" heigth="300" src="build/img/thumb/${i}.jpg" alt="Imagen galeria">
-        `;
+<source srcset="build/img/thumb/${i}.jpeg" type="image/jpeg">
+<source srcset="build/img/thumb/${i}.webp" type="image/webp">
+<img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+    `;
+
+        imagen.onclick = function(){
+            mostrarImagen(i)
+        };
+
+        galeria.appendChild(imagen);
+
     }
 }
 
-function mostrarImagen() {
-  
+function mostrarImagen(id) {
+
+    const imagen = document.createElement('picture');
+
+    imagen.innerHTML = `
+    <source srcset="build/img/grande/${i}.jpeg" type="image/jpeg">
+    <source srcset="build/img/grande/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/grande/${i}.jpg" alt="imagen galeria">
+        `;
+
+    const overlay = document.createElement('div');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    overlay.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+
+    const cerrarModal = document.createElement('p');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+     
 }
