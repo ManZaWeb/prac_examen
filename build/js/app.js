f460ee143a19636c3cd922e5dda9252c57a10b40
Mario <maalnu93@hotmail.com>
2024-02-16 12:27:54 +0100

    Empezando Galeria

--- build/js/app.js
+++ build/js/app.js
@@ -12,7 +12,16 @@ function iniciarApp() {
 }
 
 function crearGaleria() {
-  
+  const galeria = document.querySelector('.galeria-imagenes');
+
+    for (let i = 1; i <= 12; i++) {
+      const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+            <source srcset="build/img/thumb/${i}.jpg" type="image/jpeg">
+            <img loading="lazy" width="200" heigth="300" src="build/img/thumb/${i}.jpg" alt="Imagen galeria">
+        `;
+    }
 }
 
 function mostrarImagen() {
